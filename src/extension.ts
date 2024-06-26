4cdb5f69c401be9bcd6a25fc9e19a0193d9e5057
RayanDri <rayan.drissi@icloud.com>
2024-06-26 20:23:29 +0200

    Change prompt

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,18 @@ import * as vscode from "vscode";
 import axios from "axios";
 
 export function activate(context: vscode.ExtensionContext) {
-  
-  
   vscode.window.showInformationMessage("Hello");
 
-
   // Add an icon to the status bar
-  const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-  const iconPath = vscode.Uri.file(
-    context.extensionPath + '/icon-abdel.png'
+  const myStatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    100
   );
+  const iconPath = vscode.Uri.file(context.extensionPath + "/icon-abdel.png");
 
   myStatusBarItem.text = `Abdel qui code $(file)`;
-  myStatusBarItem.tooltip = 'Abdel ne code pas Abdel dors';
-  myStatusBarItem.command = 'extension.showNotification';
+  myStatusBarItem.tooltip = "Abdel ne code pas Abdel dors";
+  myStatusBarItem.command = "extension.showNotification";
   myStatusBarItem.show();
 
   context.subscriptions.push(myStatusBarItem);
@@ -44,7 +42,7 @@ export function activate(context: vscode.ExtensionContext) {
       const text = document.getText(range);
 
       // Prepare the prompt
-      const prompt = `Complete the following code while maintaining the existing style and logic. If it's Python, ensure PEP 8 compliance:\n\n${text}\n`;
+      const prompt = `Complete the following code while maintaining the existing style and logic. If it's Python, ensure PEP 8 compliance (Give only code it's directly write in .py file, don't give comment):\n\n${text}\n`;
       try {
         const apiKey = await vscode.window.showInputBox({
           prompt: "Enter your OpenAI API key",
